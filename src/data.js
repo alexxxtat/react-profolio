b1dcb064782b80445c4c2c8a43306da6847879a1
Alex Chou <alexchtat@gmail.com>
2022-07-23 20:20:10 -0700

    Done the Product Component

---
+++ src/data.js
@@ -0,0 +1,33 @@
+//TODO: add the data
+export const products = [
+  {
+    id: 1,
+    img: "",
+    link: "",
+  },
+  {
+    id: 2,
+    img: "",
+    link: "",
+  },
+  {
+    id: 3,
+    img: "",
+    link: "",
+  },
+  {
+    id: 4,
+    img: "",
+    link: "",
+  },
+  {
+    id: 5,
+    img: "",
+    link: "",
+  },
+  {
+    id: 6,
+    img: "",
+    link: "",
+  },
+];
